5d082f0b21f2c2db4c52bb7c73a9682f91a2d54f
髙原悠加(k22m2022) <k22m2022@cis.twcu.ac.jp>
2025-07-31 00:11:19 +0900

    Update utils.py

--- SingularTrajectory/utils/utils.py
+++ SingularTrajectory/utils/utils.py
@@ -4,10 +4,8 @@ import random
 import numpy as np
 import torch
 
-
 def reproducibility_settings(seed: int = 0):
     r"""Set the random seed for reproducibility"""
-
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
@@ -16,10 +14,8 @@ def reproducibility_settings(seed: int = 0):
     torch.backends.cuda.matmul.allow_tf32 = False  # Settings for 3090
     torch.backends.cudnn.allow_tf32 = False  # Settings for 3090
 
-
 def get_exp_config(file: str):
     r"""Load the configuration files"""
-
     assert os.path.exists(file), f"File {file} does not exist!"
     file = open(file)
     config = json.load(file)
@@ -28,31 +24,25 @@ def get_exp_config(file: str):
             config[k] = DotDict(config[k])
     return DotDict(config)
 
-
 class DotDict(dict):
     r"""dot.notation access to dictionary attributes"""
-
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
     __getstate__ = dict
     __setstate__ = dict.update
 
-
 def print_arguments(args, length=100, sep=': ', delim=' | '):
     r"""Print the arguments in a nice format
-
     Args:
         args (dict): arguments
         length (int): maximum length of each line
         sep (str): separator between key and value
         delim (str): delimiter between lines
     """
-
     text = []
     for key in args.keys():
         text.append('{}{}{}'.format(key, sep, args[key]))
-
     cl = 0
     for n, line in enumerate(text):
         if cl + len(line) > length:
@@ -65,26 +55,26 @@ def print_arguments(args, length=100, sep=': ', delim=' | '):
             cl += len(delim)
     print('')
 
-
 def augment_trajectory(obs_traj, pred_traj, flip=True, reverse=True):
     r"""Flip and reverse the trajectory
-
     Args:
         obs_traj (torch.Tensor): observed trajectory with shape (num_peds, obs_len, 2)
         pred_traj (torch.Tensor): predicted trajectory with shape (num_peds, pred_len, 2)
         flip (bool): whether to flip the trajectory
         reverse (bool): whether to reverse the trajectory
     """
-
     if flip:
-        obs_traj = torch.cat([obs_traj, obs_traj * torch.FloatTensor([[[1, -1]]])], dim=0)
-        pred_traj = torch.cat([pred_traj, pred_traj * torch.FloatTensor([[[1, -1]]])], dim=0)
+        # デバイスを取得して統一する
+        device = obs_traj.device
+        flip_tensor = torch.FloatTensor([[[1, -1]]]).to(device)
+        
+        obs_traj = torch.cat([obs_traj, obs_traj * flip_tensor], dim=0)
+        pred_traj = torch.cat([pred_traj, pred_traj * flip_tensor], dim=0)
     elif reverse:
         full_traj = torch.cat([obs_traj, pred_traj], dim=1)  # NTC
         obs_traj = torch.cat([obs_traj, full_traj.flip(1)[:, :obs_traj.size(1)]], dim=0)
         pred_traj = torch.cat([pred_traj, full_traj.flip(1)[:, obs_traj.size(1):]], dim=0)
     return obs_traj, pred_traj
-
 
 if __name__ == '__main__':
     cfg = get_exp_config("../config/stochastic/singulartrajectory-transformerdiffusion-eth.json")
